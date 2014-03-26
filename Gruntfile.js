61b74ac3b7909728c30fe63ddae299624160912f
Viktor Dorotovic <viktor@dorotovic.name>
2014-03-26 20:53:30 +0000

    Grunt for yuidoc

---
+++ Gruntfile.js
@@ -0,0 +1,32 @@
+module.exports = function(grunt) {
+
+  grunt.initConfig({
+    pkg: grunt.file.readJSON('package.json'),
+    yuidoc: {
+      compile: {
+        name: '<%= pkg.name %>',
+        description: '<%= pkg.description %>',
+        version: '<%= pkg.version %>',
+        url: '<%= pkg.homepage %>',
+        options: {
+          paths: 'lib/outwave',
+          outdir: 'doc/api'
+        }
+      }
+    },
+    clean: ['doc/api']
+  });
+
+  grunt.loadNpmTasks('grunt-contrib-yuidoc');
+
+  grunt.loadNpmTasks('grunt-contrib-clean');
+
+
+/*
+  grunt.registerTask('test', ['jshint', 'qunit']);
+
+  grunt.registerTask('default', ['jshint', 'qunit', 'concat', 'uglify']);
+*/
+  grunt.registerTask('doc',['yuidoc']);
+
+};
